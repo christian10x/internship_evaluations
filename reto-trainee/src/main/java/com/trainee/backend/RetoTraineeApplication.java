bd2b7647b2802a61e9527e2a435b9abe6da314d3
jhonOrr <jorregoj@outlook.com>
2024-09-20 13:37:55 -0500

    first commit

---
+++ reto-trainee/src/main/java/com/trainee/backend/RetoTraineeApplication.java
@@ -0,0 +1,13 @@
+package com.trainee.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoTraineeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoTraineeApplication.class, args);
+	}
+
+}
