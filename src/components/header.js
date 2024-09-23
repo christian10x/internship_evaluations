c5cba4be1e07858c0d9e387d2c5f1c6fdff3b7db
Morelia <morelia.gonzales.v@gmail.com>
2024-09-23 07:57:14 -0500

    imageSlider y header

--- src/components/header.js
+++ src/components/header.js
@@ -1,45 +1,25 @@
-import React from 'react';
-import './horario.css';
+// src/components/Header.js
+import React from "react";
+import './header.css';
 
-function Horario() {
+function Header() {
     return (
-        <div className="horario-container">
-            <div className="horario-header">
-                <h3>Hoy, 21 de sep 2024</h3>
-                <i className="fa-solid fa-calendar-days"></i>
-            </div>
+        <header className="header">
+            <img src="/img/intranet_logo.png" alt="UTP+Intranet Logo" />
 
-            <div className="horario-item presencial">
-                <p className="badge presencial">Presencial</p>
-                <p className="curso-titulo">Curso: Programación de Interfaces y Dispositivos Periféricos</p>
-                <p className="horario-hora">14:30 - 16:45</p>
-                <div className="horario-lugar">
-                    <a href="#" className="lugar-link">
-                        <i className="fa-solid fa-location-dot"></i> Jr. Hernán Velarde 289
-                    </a>
-                    <span className="aula">B0307</span>
+            <div className="user-info">
+                <i class="fa-regular fa-bell text-3xl pr-[20px]"></i>
+                <div className="estudiante">
+                    <p>Hola, Morelia Paola</p>
+                    <p>Estudiante</p>
                 </div>
+                
+                <img src="/img/logo_perfil.png" alt="User Avatar" className="avatar" />
+                <img src="/img/arrow_down.png" alt="Flecha abajo" className="flecha" />
             </div>
-
-            <div className="horario-item virtual">
-                <p className="badge virtual">Virtual en vivo</p>
-                <p className="curso-titulo">Curso: Desarrollo de Software II</p>
-                <p className="horario-hora">14:30 - 16:45</p>
-                <a href="#" className="horario-zoom">
-                    <i className="fa-solid fa-video"></i> Unirme a zoom
-                </a>
-            </div>
-
-            <div className="horario-item virtual-247">
-                <p className="badge virtual-247">Virtual 24/7</p>
-                <p className="curso-titulo">Tienes 2 cursos</p>
-                <p className="horario-hora">Disponible 24 horas durante el ciclo</p>
-                <a href="#" className="horario-utpclass">
-                    <i className="fa-solid fa-laptop"></i> UTP+class
-                </a>
-            </div>
-        </div>
+            
+        </header>
     );
 }
 
-export default Horario;
+export default Header;
