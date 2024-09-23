7b40092d222f8cfaa709525acd8b964d810e4def
Morelia <morelia.gonzales.v@gmail.com>
2024-09-23 10:26:12 -0500

    Navegador funciona correctamente

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,36 +1,39 @@
+// src/components/navbar.js
 import React from "react";
-import "./navbar.css";
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';  // Importar useLocation
+import './navbar.css';
 
 function Navbar() {
-    return (
-        <menu class="navbar">
-            <div class="navbar-item">
-                <i class="fa-solid fa-bars"></i>
-            </div>
+  const location = useLocation();  // Obtener la ruta actual
 
-            <Link to="/home" className="navbar-item home active">
-                <i class="fa-solid fa-house"></i>
-                <p className="active">Inicio</p>
-            </Link>
+  return (
+    <menu className="navbar">
+      <div className="navbar-item">
+        <i className="fa-solid fa-bars"></i>
+      </div>
 
-            <Link to="/calendar" class="navbar-item">
-                <i class="fa-solid fa-calendar"></i>
-                <p>Calendario</p>
-            </Link>
+      <Link to="/" className={`navbar-item ${location.pathname === '/' ? 'active' : ''}`}>
+        <i className="fa-solid fa-house"></i>
+        <p className={location.pathname === '/' ? 'active' : ''}>Inicio</p>
+        </Link>
 
-            <Link to="/courses" class="navbar-item">
-                <i class="fa-solid fa-book"></i>
-                <p>Cursos</p>
-            </Link>
 
-            <Link to="/payments" class="navbar-item">
-                <i class="fa-solid fa-house"></i>
-                <p>Pagos</p>
-            </Link>
-        </menu>
+      <Link to="/calendar" className={`navbar-item ${location.pathname === '/calendar' ? 'active' : ''}`}>
+        <i className="fa-solid fa-calendar"></i>
+        <p>Calendario</p>
+      </Link>
 
-    );
+      <Link to="/courses" className={`navbar-item ${location.pathname === '/courses' ? 'active' : ''}`}>
+        <i className="fa-solid fa-book"></i>
+        <p>Cursos</p>
+      </Link>
+
+      <Link to="/payments" className={`navbar-item ${location.pathname === '/payments' ? 'active' : ''}`}>
+        <i className="fa-solid fa-wallet"></i>
+        <p>Pagos</p>
+      </Link>
+    </menu>
+  );
 }
 
 export default Navbar;
--- src/pages/home.js
+++ src/pages/home.js
@@ -5,14 +5,12 @@ import CursosCiclo from "../components/cursosCiclo";
 import ResumenGeneral from "../components/resumenGeneral";
 import Interes from "../components/interes";
 import Horario from "../components/horario";
-import '../css/homepage.css';
+import '../css/homepage.css';  // Make sure this is at the top
 
 function HomePage() {
   return (
     <>
-    
       <div className="content">
-       
         <ImageSlider />
         <div className="main_content">
           <div className="izquierda">
@@ -23,20 +21,15 @@ function HomePage() {
                 <ResumenGeneral />
               </div>
               <div className="interes">
-              <Interes />
+                <Interes />
               </div>
-              
             </div>
-            
           </div>
           <div className="horario">
             <Horario />
           </div>
         </div>
-        
       </div>
-      
-      
     </>
   );
 }
