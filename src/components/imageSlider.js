10f411518895bb7c74ea8eb1af3a35a44a8cb577
Morelia <morelia.gonzales.v@gmail.com>
2024-09-23 07:24:50 -0500

    imageSlider listo

--- src/components/imageSlider.js
+++ src/components/imageSlider.js
@@ -1,11 +1,48 @@
-import React from "react";
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination'; 
+
+import { Pagination, Autoplay } from 'swiper/modules'; // Import Autoplay
+
 import './imageSlider.css';
 
 function ImageSlider() {
     return (
-        <div className="image-slider">
-            <img src="/path/to/slider-image.png" alt="Slider" />
-        </div>
+        <Swiper
+            modules={[Pagination, Autoplay]}  // Add Autoplay module
+            spaceBetween={30}
+            centeredSlides={true}
+            autoplay={{ 
+                delay: 3000,  // 3 seconds delay between slides
+                disableOnInteraction: false // Keeps autoplay running after interaction
+            }}
+            pagination={{ clickable: true }}
+            loop={true}  // Enables looping of slides
+        >
+            <SwiperSlide>
+                <div className="slide-content">
+                    <img src="/img/banner1.png" alt="Slide 1" className="carousel-image" />
+                </div>
+            </SwiperSlide>
+
+            <SwiperSlide>
+                <div className="slide-content">
+                    <img src="/img/banner2.jpg" alt="Slide 1" className="carousel-image" />
+                </div>
+            </SwiperSlide>
+            <SwiperSlide>
+                <div className="slide-content">
+                    <img src="/img/banner3.jpg" alt="Slide 1" className="carousel-image" />
+                </div>
+            </SwiperSlide>
+            <SwiperSlide>
+                <div className="slide-content">
+                    <img src="/img/banner4.jpg" alt="Slide 1" className="carousel-image" />
+                </div>
+            </SwiperSlide>
+
+        </Swiper>
     );
 }
 
